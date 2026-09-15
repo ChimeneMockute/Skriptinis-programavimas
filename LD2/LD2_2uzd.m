ad6d9d9122c5063c07b21e70b3e75dd765952a89
ChimeneMockute <chimene.mockute@stud.vilniustech.lt>
2026-09-15 13:28:51 +0300

    Add files via upload

---
+++ LD2/LD2_2uzd.m
@@ -0,0 +1,17 @@
+% Chimene Mockute 
+% EEf - 25/2
+% 2026-09-15
+% masyvo elementu indeksavimas
+
+
+clear;
+
+% ivedimas
+A = input('Iveskite vektoriu A ');
+
+% Pakartojami elementai po 4 kartus
+B = repelem(A, 4);
+
+% Isvedimas
+disp('vektorius B yra:');
+disp(B);
